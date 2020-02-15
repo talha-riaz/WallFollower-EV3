ec222aba87c7b99936d2ce9a60738eba5ab78199
Talha Riaz <55361640+talha-riaz@users.noreply.github.com>
2020-02-15 05:00:28 -0500

    Update UltrasonicController.java

--- project/UltrasonicController.java
+++ project/UltrasonicController.java
@@ -1,6 +1,6 @@
-package ca.mcgill.ecse211.project;
+package project;
 
-import static ca.mcgill.ecse211.project.Resources.*;
+import static project.Resources.*;
 
 /**
  * Controls the robot's movements based on ultrasonic data.
